e1ccd08459eab477449c23c9d8aafd2f63f5deaf
sayman <mdrabbi.asm@gmail.com>
2022-02-10 12:24:28 +0600

    add account

---
+++ account.js
@@ -0,0 +1,43 @@
+let deposit = document.getElementById('Deposit');
+const withdraw = document.getElementById('Withdraw');
+const balance = document.getElementById('Balance');
+const depositInput = document.getElementById('Deposit-input');
+const returne = document.getElementById('returne');
+const depositButton = document.getElementById('Deposit-button');
+const  withdrawInput = document.getElementById('Withdraw-input');
+const withdrawButton = document.getElementById('Withdraw-button');
+const returne2 = document.getElementById('returne2');
+// =====Deposit====
+
+depositButton.addEventListener('click',function(e){
+    let depositNew = 0;
+    let balanceNew = parseFloat(balance.innerText)
+    let depositValue =depositInput.value;
+    if((depositValue === '') || isNaN(depositValue)){
+     returne.innerText = "Please Input some Value"
+     returne.style.color="red";
+     returne.style.marginBottom = "10px"
+    }
+    else{
+        depositNew = parseFloat(deposit.innerText);
+        deposit.innerText = depositNew + parseFloat(depositValue);
+         balance.innerText = balanceNew + parseFloat(depositValue);
+         depositInput.value = '';
+         
+    }
+})
+// === withdraw====
+withdrawButton.addEventListener('click',function(e){
+    let balanceNew2 = parseFloat(balance.innerText)
+    if((withdrawInput.value === '') || isNaN(withdrawInput.value)){
+        returne2.innerText = "Please Input some Value"
+        returne2.style.color="red";
+        returne2.style.marginBottom = "10px"
+       }
+       else{
+           withdraw.innerText = withdrawInput.value;
+           console.log(parseFloat(withdrawInput.value))
+           balance.innerText = balanceNew2 - parseFloat(withdrawInput.value);
+           withdrawInput.value = '';
+       }
+})
